a4873dff20e67a971d08cae25d7046c0e50aac97
Panos Hatzinikolaou <pjhatzis@gmail.com>
2020-05-03 17:48:53 -0700

    added product image to project

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const app = new Vue({
   el: '#app',
   data: {
     product: 'Socks',
-    description: 'Warm fuzzy socks, that hug your feet, and give them colour.'
+    description: 'Warm fuzzy socks, that hug your feet, and give them colour.',
+    image: './socks.jpg'
   }
 })
