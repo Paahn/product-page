b703192f3ab2de0c041e745125e236dd3e6c5516
Panos Hatzinikolaou <pjhatzis@gmail.com>
2020-05-04 23:52:29 -0700

    added on sale tag to product

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const app = new Vue({
     description: 'Warm fuzzy socks, that hug your feet, and give them colour.',
     image: './socks.jpg',
     link: 'https://www.amazon.ca/s?k=socks&ref=nb_sb_noss_2',
-    inventory: 50
+    inventory: 50,
+    onSale: true
   }
 })
