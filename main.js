b18a07440bd3068d4915971fc7fb6a1afc5d4623
Panos Hatzinikolaou <pjhatzis@gmail.com>
2020-05-05 17:30:11 -0700

    adding colour variants on webpage

--- main.js
+++ main.js
@@ -7,6 +7,16 @@ const app = new Vue({
     link: 'https://www.amazon.ca/s?k=socks&ref=nb_sb_noss_2',
     inventory: 50,
     onSale: true,
-    details: ["80% cotton", "10% pineapple", "10% rabbit", "all-purpose"]
+    details: ["80% cotton", "10% pineapple", "10% rabbit", "all-purpose"],
+    variants: [
+      {
+        variantId: 2234,
+        variantColour: "green"
+      },
+      {
+        variantId: 2235,
+        variantColour: "blue"
+      }
+    ]
   }
 })
