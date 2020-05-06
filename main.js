5b51c6944a4ca25cd8592b2c65a630a970e32b3b
Panos Hatzinikolaou <pjhatzis@gmail.com>
2020-05-06 15:11:55 -0700

    class binding the disabled button style to the add to cart button when inventory reaches zero

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const app = new Vue({
     description: 'Warm fuzzy socks, that hug your feet, and give them colour.',
     image: './socks.jpg',
     link: 'https://www.amazon.ca/s?k=socks&ref=nb_sb_noss_2',
-    inventory: 50,
+    inventory: 10,
     onSale: true,
     details: ["80% cotton", "10% pineapple", "10% rabbit", "all-purpose"],
     variants: [
