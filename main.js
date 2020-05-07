c3fb6b5889d2c6c506d9a118bcced39db75888e9
Panos Hatzinikolaou <pjhatzis@gmail.com>
2020-05-07 16:07:23 -0700

    added required attribute to form

--- main.js
+++ main.js
@@ -3,17 +3,17 @@ Vue.component('product-review', {
   <form class="review-form" @submit.prevent="onSubmit">
   <p>
     <label for="name">Name: </label>
-    <input id="name" v-model="name">
+    <input id="name" v-model="name" required>
   </p>
 
   <p>
     <label for="name">Review: </label>
-    <textarea id="review" v-model="review"></textarea>
+    <textarea id="review" v-model="review" required></textarea>
   </p>
 
   <p>
     <label for="rating">Rating: </label>
-    <select id="rating" v-model.number="rating">
+    <select id="rating" v-model.number="rating" required>
       <option>5</option>
       <option>4</option>
       <option>3</option>
