997d06fff3de25abef47c94538bc05a57cb970a8
Panos Hatzinikolaou <pjhatzis@gmail.com>
2020-05-06 17:30:55 -0700

    refactored code, moved the product div into a Vue component

--- main.js
+++ main.js
@@ -1,29 +1,68 @@
-const app = new Vue({
-  el: '#app',
-  data: {
-    brand: 'Master of Puppets',
-    product: 'Socks',
-    description: 'Warm fuzzy socks, that hug your feet, and give them colour.',
-    selectedVariant: 0,
-    link: 'https://www.amazon.ca/s?k=socks&ref=nb_sb_noss_2',
-    onSale: true,
-    details: ["80% cotton", "10% pineapple", "10% rabbit", "all-purpose"],
-    variants: [
-      {
-        variantId: 2234,
-        variantColour: 'green',
-        variantImage: './socks.jpg',
-        variantQuantity: 23
-      },
-      {
-        variantId: 2235,
-        variantColour: "blue",
-        variantImage: './blue-socks.jpg',
-        variantQuantity: 0
-      }
-    ],
-    sizes: ["freakishly small", "small", "medium", "large", "sasquatch"],
-    cart: 0
+Vue.component('product', {
+  template: `
+  <div class="product">
+        <div class="product-image">
+          <img :src="image" alt="socks">
+        </div>
+        <h1>
+          {{ title }}
+        </h1>
+        <p v-if="inventory > 10">In Stock</p>
+        <p v-else-if=" inventory <= 10 && inventory > 0">Few items left!</p>
+        <p v-else :class="{outStock: inventory<=0}">Out of Stock</p>
+        <span v-if="onSale">On Sale!!!</span>
+        <ul>
+          <li v-for="detail in details"> {{ detail }} </li>
+        </ul>
+        <div v-for="(variant, index) in variants"
+             :key="variant.variantId"
+             class="color-box"
+             :style="{backgroundColor: variant.variantColour}"
+             @mouseover="updateProduct(index)">
+        </div>
+        <ul>
+          <li v-for="size in sizes">{{ size }}</li>
+        </ul>
+        <p>
+          {{ description }}
+        </p>
+        <a :href="link" target="_blank">More products like this</a>
+        <button @click="addToCart"
+        v-if="inventory>0">Add to cart</button>
+        <button @click="removeFromCart"
+        v-if="inventory>0">Remove from cart</button>
+
+        <div class="cart">
+          <p>Cart({{cart}})</p>
+        </div>
+      </div>
+  `,
+  data() {
+    return {
+      brand: 'Master of Puppets',
+      product: 'Socks',
+      description: 'Warm fuzzy socks, that hug your feet, and give them colour.',
+      selectedVariant: 0,
+      link: 'https://www.amazon.ca/s?k=socks&ref=nb_sb_noss_2',
+      onSale: true,
+      details: ["80% cotton", "10% pineapple", "10% rabbit", "all-purpose"],
+      variants: [
+        {
+          variantId: 2234,
+          variantColour: 'green',
+          variantImage: './socks.jpg',
+          variantQuantity: 23
+        },
+        {
+          variantId: 2235,
+          variantColour: "blue",
+          variantImage: './blue-socks.jpg',
+          variantQuantity: 0
+        }
+      ],
+      sizes: ["freakishly small", "small", "medium", "large", "sasquatch"],
+      cart: 0
+    }
   },
   methods: {
     addToCart() {
@@ -47,4 +86,10 @@ const app = new Vue({
       return this.variants[this.selectedVariant].variantQuantity
     }
   }
+})
+
+
+
+const app = new Vue({
+  el: '#app'
 })
